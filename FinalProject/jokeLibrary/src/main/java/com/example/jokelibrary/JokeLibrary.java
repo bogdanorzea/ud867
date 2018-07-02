cd213c0581fb5bb905a62bd520ddbfcc4e7ae1b1
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-07-02 23:44:48 +0300

    Add a Java joke library to the project

---
+++ FinalProject/jokeLibrary/src/main/java/com/example/jokelibrary/JokeLibrary.java
@@ -0,0 +1,9 @@
+package com.example.jokelibrary;
+
+public class JokeLibrary {
+
+    public static String getJoke() {
+        return "When Bill Gates donates 30% of his net worth he is praised as a generous hero, " +
+                "but when I do it people tell me they don't accept donations under a dollar.";
+    }
+}
