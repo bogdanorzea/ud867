8eacb82b305b236d52b8cc216dadb990ce2c5964
Bogdan Orzea <bogdan.orzea@gmail.com>
2018-07-03 00:20:37 +0300

    Add a Android library to display jokes

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,13 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.jokelibrary.JokeLibrary;
+import com.example.jokelibraryactivity.JokeActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -42,7 +43,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, JokeLibrary.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokeActivity.class);
+        intent.putExtra("joke", JokeLibrary.getJoke());
+
+        startActivity(intent);
     }
 
 
